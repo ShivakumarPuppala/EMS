6a8f807f4546e9eae4407e32d8ac799cd31bc76e
ShivakumarPuppala <nagar3878@gmail.com>
2024-12-20 18:08:00 +0530

    "Intial commit"

---
+++ ems-frontend/src/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+import EmployeeComponent from './Components/EmployeeComponent'
+import FooterComponent from './Components/FooterComponent'
+import HeaderComponent from './Components/HeaderComponent'
+import ListEmployeeComponent from './Components/ListEmployeeComponent'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+
+function App() {
+
+
+  return (
+    <>
+    <BrowserRouter>
+    <HeaderComponent />
+    <Routes>
+      <Route path='/'element={<ListEmployeeComponent/>}></Route>
+      <Route path='/employees' element={<ListEmployeeComponent/>}></Route>
+      <Route path='/add-employee' element={<EmployeeComponent />}></Route>
+      <Route path='/edit-employee/:id' element={<EmployeeComponent />}></Route>
+    </Routes>
+  
+  <FooterComponent />
+  </BrowserRouter>
+    </>
+  )
+}
+
+export default App
